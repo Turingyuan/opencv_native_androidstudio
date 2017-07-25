1f49256441d44824ca9138497b6e816047d78bfc
John Hany <johnhany@163.com>
2017-07-25 19:02:29 +0800

    Update to AS 2.3.3 & OCV 3.2

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,18 +1,18 @@
 #include <jni.h>
 #include <string>
 #include <opencv2/core/core.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/features2d/features2d.hpp>
 
 using namespace std;
 using namespace cv;
 
 extern "C"
 {
-void JNICALL Java_ch_hepia_iti_opencvnativeandroidstudio_MainActivity_salt(JNIEnv *env, jobject instance,
-                                                                           jlong matAddrGray,
-                                                                           jint nbrElem) {
+void JNICALL Java_net_johnhany_opencv_1ndk_1camera_MainActivity_salt(JNIEnv *env, jobject instance,
+                                                             jlong matAddrGray,
+                                                             jint nbrElem) {
+
     Mat &mGr = *(Mat *) matAddrGray;
+
     for (int k = 0; k < nbrElem; k++) {
         int i = rand() % mGr.cols;
         int j = rand() % mGr.rows;
@@ -20,3 +20,4 @@ void JNICALL Java_ch_hepia_iti_opencvnativeandroidstudio_MainActivity_salt(JNIEn
     }
 }
 }
+
